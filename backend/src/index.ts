c6cc4721996be196cc376d7f771e9c9b967c5bf7
OleksandrHridzhak <sgridzak@gmail.com>
2026-09-15 12:22:06 +0300

    chore: cors setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import quizRoutes from './quiz.routes';
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
